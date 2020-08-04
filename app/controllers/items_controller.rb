9c1b3857532b4686d83c0563b9fe38d66a5eec87
switch023 <yuuhamada23@gmail.com>
2020-08-04 14:06:54 +0900

    Item-Exhibition

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    # @item = Item.all
+  end
+
+  def new
+    # @item = Item.new
+  end
+
+  def create
+    # Item.crerate(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).parmit(:name, :introduction, :price, :brand_id, :category_id, :item_condition_id,
+                                  :postage_payer_id, :prefecture_code, :prepation_day_id, :postage_type_id)
   end
 end
