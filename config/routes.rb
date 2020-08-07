3dc984cae93fb0e0bba84a32ea9cac81f78360af
munehiro-0306 <mutan0306@yahoo.co.jp>
2020-08-07 16:48:39 +0900

    Frontview-login-signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  devise_for :users
+  get 'signup/index'
+  root "signup#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
