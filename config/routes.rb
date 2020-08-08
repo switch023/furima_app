a48d8a7f3cdff59e360ed9a80bdda9d049753069
r.y <itworldbird@gmail.com>
2020-08-08 20:05:17 +0900

    mypage-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'card/new'
+  get 'card/show'
   root 'items#index'
+  resources :mypages, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
