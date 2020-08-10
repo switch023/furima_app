6b827d99f73fc79d6880abfe5a1b44a8240e050a
r.y <itworldbird@gmail.com>
2020-08-10 21:09:00 +0900

    route.rb-fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:new, :create]
   devise_for :users
-  get 'card/new'
-  get 'card/show'
-  root 'items#index'
+  resources :card, only: [:new, :show]
   resources :mypages, only: [:index]
 end
