ace333b284896f66d24df713a6e016583561f23d
switch023 <yuuhamada23@gmail.com>
2020-08-15 15:24:39 +0900

    css fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :signup, only: [:index]
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items, only: [:new, :create, :show]
   devise_for :users
