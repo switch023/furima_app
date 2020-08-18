aec2f6f44b9aff4ee7970fbf876fa51fad52d421
r.y <itworldbird@gmail.com>
2020-08-18 21:44:44 +0900

    table-create

---
+++ db/migrate/20200818123242_create_items_images.rb
@@ -0,0 +1,9 @@
+class CreateItemsImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items_images do |t|
+      t.references      :items, null :false, foregin_key :true
+      t.string          :url,   null :false
+      t.timestamps
+    end
+  end
+end
