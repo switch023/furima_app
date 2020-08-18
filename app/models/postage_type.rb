ff8e332376a8d9fc7053e399b8a2c657eed96e20
switch023 <yuuhamada23@gmail.com>
2020-08-18 21:36:08 +0900

    tabel create

--- app/models/postage_type.rb
+++ app/models/postage_type.rb
@@ -1,5 +1,7 @@
-class Prefecturecode < ActiveHash::Base
+class Postage_type < ActiveHash::Base
   self.data = [
-
+    {id: 1, name: 'レターパックライト'},{id: 2, name: 'ネコポス'},
+    {id: 3, name: 'ゆうパケット'},{id: 4, name: '日本郵便'},
+    {id: 5, name: 'ヤマト便'}
   ]
 end
