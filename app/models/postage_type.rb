b0725d85cfd82159769e17b94980eeca2bfc221c
switch023 <yuuhamada23@gmail.com>
2020-08-18 21:58:12 +0900

    fix table

--- app/models/postage_type.rb
+++ app/models/postage_type.rb
@@ -1,7 +1,7 @@
 class Postage_type < ActiveHash::Base
   self.data = [
-    {id: 1, name: 'レターパックライト'},{id: 2, name: 'ネコポス'},
-    {id: 3, name: 'ゆうパケット'},{id: 4, name: '日本郵便'},
-    {id: 5, name: 'ヤマト便'}
+    {id: 1, postagetype: 'レターパックライト'},{id: 2, postagetype: 'ネコポス'},
+    {id: 3, postagetype: 'ゆうパケット'},{id: 4, postagetype: '日本郵便'},
+    {id: 5, postagetype: 'ヤマト便'}
   ]
 end
--- app/models/prefecture_code.rb
+++ app/models/prefecture_code.rb
@@ -1,20 +1,20 @@
 class Prefecture_code < ActiveHash::Base
   self.data = [
-    {id: 1, name: '北海道'}, {id: 2, name: '青森県'}, {id: 3, name: '岩手県'},
-    {id: 4, name: '宮城県'}, {id: 5, name: '秋田県'}, {id: 6, name: '山形県'},
-    {id: 7, name: '福島県'}, {id: 8, name: '茨城県'}, {id: 9, name: '栃木県'},
-    {id: 10, name: '群馬県'}, {id: 11, name: '埼玉県'}, {id: 12, name: '千葉県'},
-    {id: 13, name: '東京都'}, {id: 14, name: '神奈川県'}, {id: 15, name: '新潟県'},
-    {id: 16, name: '富山県'}, {id: 17, name: '石川県'}, {id: 18, name: '福井県'},
-    {id: 19, name: '山梨県'}, {id: 20, name: '長野県'}, {id: 21, name: '岐阜県'},
-    {id: 22, name: '静岡県'}, {id: 23, name: '愛知県'}, {id: 24, name: '三重県'},
-    {id: 25, name: '滋賀県'}, {id: 26, name: '京都府'}, {id: 27, name: '大阪府'},
-    {id: 28, name: '兵庫県'}, {id: 29, name: '奈良県'}, {id: 30, name: '和歌山県'},
-    {id: 31, name: '鳥取県'}, {id: 32, name: '島根県'}, {id: 33, name: '岡山県'},
-    {id: 34, name: '広島県'}, {id: 35, name: '山口県'}, {id: 36, name: '徳島県'},
-    {id: 37, name: '香川県'}, {id: 38, name: '愛媛県'}, {id: 39, name: '高知県'},
-    {id: 40, name: '福岡県'}, {id: 41, name: '佐賀県'}, {id: 42, name: '長崎県'},
-    {id: 43, name: '熊本県'}, {id: 44, name: '大分県'}, {id: 45, name: '宮崎県'},
-    {id: 46, name: '鹿児島県'}, {id: 47, name: '沖縄県'}
+    {id: 1, prefecturecode: '北海道'}, {id: 2, prefecturecode: '青森県'}, {id: 3, prefecturecode: '岩手県'},
+    {id: 7, prefecturecode: '福島県'}, {id: 8, prefecturecode: '茨城県'}, {id: 9, prefecturecode: '栃木県'},
+    {id: 10, prefecturecode: '群馬県'}, {id: 11, prefecturecode: '埼玉県'}, {id: 12, prefecturecode: '千葉県'},
+    {id: 13, prefecturecode: '東京都'}, {id: 14, prefecturecode: '神奈川県'}, {id: 15, prefecturecode: '新潟県'},
+    {id: 16, prefecturecode: '富山県'}, {id: 17, prefecturecode: '石川県'}, {id: 18, prefecturecode: '福井県'},
+    {id: 19, prefecturecode: '山梨県'}, {id: 20, prefecturecode: '長野県'}, {id: 21, prefecturecode: '岐阜県'},
+    {id: 22, prefecturecode: '静岡県'}, {id: 23, prefecturecode: '愛知県'}, {id: 24, prefecturecode: '三重県'},
+    {id: 25, prefecturecode: '滋賀県'}, {id: 26, prefecturecode: '京都府'}, {id: 27, prefecturecode: '大阪府'},
+    {id: 28, prefecturecode: '兵庫県'}, {id: 29, prefecturecode: '奈良県'}, {id: 30, prefecturecode: '和歌山県'},
+    {id: 31, prefecturecode: '鳥取県'}, {id: 32, prefecturecode: '島根県'}, {id: 33, prefecturecode: '岡山県'},
+    {id: 34, prefecturecode: '広島県'}, {id: 35, prefecturecode: '山口県'}, {id: 36, prefecturecode: '徳島県'},
+    {id: 37, prefecturecode: '香川県'}, {id: 38, prefecturecode: '愛媛県'}, {id: 39, prefecturecode: '高知県'},
+    {id: 40, prefecturecode: '福岡県'}, {id: 41, prefecturecode: '佐賀県'}, {id: 42, prefecturecode: '長崎県'},
+    {id: 43, prefecturecode: '熊本県'}, {id: 44, prefecturecode: '大分県'}, {id: 45, prefecturecode: '宮崎県'},
+    {id: 46, prefecturecode: '鹿児島県'}, {id: 47, prefecturecode: '沖縄県'}
   ]
 end
+prefecture_code
