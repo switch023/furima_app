2a4a4542ce2973e97cd6967b1dd6677cc2ded5ea
nakajinsan <yukio28301030@gmail.com>
2020-08-19 22:06:53 +0900

    items-association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :favorites
+  has_many :item_images, dependent: :destroy
+  has_one :user_evaluation
+  belongs_to :brand
+  belongs_to :category
+  belongs_to :active_hash :item_condition
+  belongs_to :active_hash :postage_payer
+  belongs_to :active_hash :prefecture_code
+  belongs_to :active_hash :preparation_day
+  belongs_to :active_hash :postage_type
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name:" User"
 end
--- db/migrate/20200818124809_create_item_images.rb
+++ db/migrate/20200818124809_create_item_images.rb
@@ -1,8 +1,8 @@
 class CreateItemImages < ActiveRecord::Migration[5.2]
   def change
     create_table :item_images do |t|
-      t.references      :items, null :false, foregin_key :true
-      t.string          :url,   null :false
+      t.references      :items, null:false, foregin_key:true
+      t.string          :url,   null:false
       t.timestamps
     end
   end
