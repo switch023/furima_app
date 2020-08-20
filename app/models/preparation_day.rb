756573cb389fdb75f232fddbf3dfc6b78e571270
r.y <itworldbird@gmail.com>
2020-08-20 20:05:32 +0900

    fixed-preparation_day

---
+++ app/models/preparation_day.rb
@@ -0,0 +1,10 @@
+class Preparation_day < ActiveHash::Base
+
+  self.data = [{id: 1, preparationday: '１〜２日で発送'},{id: 2, preparationday: '２〜３日で配送'}
+              ,{id: 3, preparationday: '４〜７日で発送'}
+  ]
+  validates :preparation_day, presence: true
+
+  include ActiveHash::Associations
+  has_many :items
+end
