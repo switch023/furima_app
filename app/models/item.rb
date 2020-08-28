f35d94b8f60112ac7d4a2e31969c773c6bee7578
nakajinsan <yukio28301030@gmail.com>
2020-08-28 12:07:14 +0900

    スペース削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :brand
   belongs_to :category
   belongs_to :buyer, class_name: "User"
-  belongs_to :seller, class_name:" User"
+  belongs_to :seller, class_name:"User"
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_condition
