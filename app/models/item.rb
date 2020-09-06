cefb2d779bbee4a9e921dee473dda62d6b26049c
switch023 <yuuhamada23@gmail.com>
2020-09-06 22:01:56 +0900

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,7 +25,7 @@ class Item < ApplicationRecord
   validates :prefecture_code_id, presence: true
   validates :preparation_day_id, presence: true
   validates :postage_type_id, presence: true
-  validates :price, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :seller_id, presence: true
 
 end
