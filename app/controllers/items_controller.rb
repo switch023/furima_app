a2760a348d24e67b57a1f09c0881f9085c708895
switch023 <yuuhamada23@gmail.com>
2020-09-09 01:06:35 +0900

    edit function init

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+
   private
   def item_params
     params.require(:item).permit( :name, :introduction, :price, :brand, :category_id,
