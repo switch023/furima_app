0338bdde7e3792e2af6e6e48db6f3c8d125c0b58
nakajinsan <yukio28301030@gmail.com>
2020-09-08 20:55:23 +0900

    item_image修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,7 +1,5 @@
 class ItemImage < ApplicationRecord
-
-  validates :item_image, presence: true
   belongs_to :item
   mount_uploader :url, ImageUploader
-  validates :url,               presence: true
-
+  validates :url, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_28_060615) do
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -27,18 +21,18 @@ ActiveRecord::Schema.define(version: 2020_08_28_060615) do
   end
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "items_id", null: false
+    t.bigint "item_id", null: false
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["items_id"], name: "index_item_images_on_items_id"
+    t.index ["item_id"], name: "index_item_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
-    t.bigint "brand_id"
+    t.text "brand"
     t.bigint "category_id", null: false
     t.bigint "item_condition_id", null: false
     t.bigint "postage_payer_id", null: false
@@ -49,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_08_28_060615) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["item_condition_id"], name: "index_items_on_item_condition_id"
