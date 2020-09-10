74f05f28f2a8627005c9261493d8c01d03483aee
switch023 <yuuhamada23@gmail.com>
2020-09-10 17:56:48 +0900

    fix

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -15,26 +15,6 @@ $(document).on('turbolinks:load', function(){
       return html;
     }
     // 投稿編集時
-    //items/:i/editページへリンクした際のアクション(1回目編集時)
-    if (window.location.href.match(/\/items\/\d+\/edit/)){
-      //登録済み画像のプレビュー表示欄の要素を取得する
-      var prevContent = $('.label-content').prev();
-      labelWidth = (660 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
-      $('.label-content').css('width', labelWidth);
-      //プレビューにidを追加
-      $('.preview-box').each(function(index, box){
-        $(box).attr('id', `preview-box__${index}`);
-      })
-      //削除ボタンにidを追加
-      $('.delete-box').each(function(index, box){
-        $(box).attr('id', `delete_btn_${index}`);
-      })
-      var count = $('.preview-box').length;
-      //プレビューが5あるときは、投稿ボックスを消しておく
-      if (count == 5) {
-        $('.label-content').hide();
-      }
-    }
     //items/:iページへリンクした際のアクション(編集エラー時)
     if (window.location.href.match(/\/items\/\d/)){
       //登録済み画像のプレビュー表示欄の要素を取得する
