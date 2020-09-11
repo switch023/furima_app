66e0379f0daeaf58ea1d1963f783ecd1471432da
munehiro-0306 <mutan0306@yahoo.co.jp>
2020-09-11 16:21:35 +0900

    修正　新規登録ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
             :family_name, :first_name_kana, :family_name_kana, :birthday, presence: true
   validates :email, confirmation: true
   validates :email, uniqueness: true
-  validates :encrypted_password,    length: { in: 7..40 }
+  validates :password, presence: true, length: {minimum: 7}
+  validates :name, format: { with: /\A[a-z0-9]+\z/ }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
