cd91babfd96710a9ac8faf8e83b378582cc6b3d5
switch023 <yuuhamada23@gmail.com>
2020-09-11 21:25:51 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   validates :email, confirmation: true
   validates :email, uniqueness: true
   validates :password, presence: true, length: {minimum: 7}
-  # validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  # validates :first_name_kana, :family_name_kana format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :first_name_kana, :family_name_kana format: { with: /\A[ァ-ヶー－]+\z/ }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
