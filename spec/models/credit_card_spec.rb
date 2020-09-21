ea62835cbf6ec1b207a1c054d05b02930620d2c4
r.y <itworldbird@gmail.com>
2020-09-21 10:36:06 +0900

    error fixed

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -6,12 +6,12 @@ describe CreditCard do
       it "カード番号がない場合" do
         card = FactoryBot.build(:credit_card, card_id: nil)
         card.valid?
-        expect(card.errors[:card_id]).to include("can`t be blank")
+        expect(card.errors[:card_id]).to include("を入力してください")
       end
       it "顧客のidがない場合" do
         card = FactoryBot.build(:credit_card, customer_id: nil)
         card.valid?
-        expect(card.errors[:customer_id]).to include("can`t be blank")
+        expect(card.errors[:customer_id]).to include("を入力してください")
       end
     end
   end
